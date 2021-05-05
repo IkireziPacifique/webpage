54551c04253e375204382e05f607c75d27188ed5
Ikirezi Pacifique <p.ikirezi@alustudent.com>
2021-05-05 14:07:39 +0200

    initial commit

---
+++ src/pages/home.vue
@@ -0,0 +1,34 @@
+<template>
+    <q-page padding>
+        <div>    
+            <div class="row">
+                <div class="col">
+                <q-header elevated class="">
+                    <q-toolbar>
+                        <q-btn flat round dense icon="message" label="Proom" class="q-mr-xs" />
+                    <q-space />
+
+                    <q-breadcrumbs active-color="grey" style="font-size: 16px">
+                        <q-breadcrumbs-el label="Course" icon="home" />
+                        <q-breadcrumbs-el label="Components" icon="widgets" />
+                        <q-breadcrumbs-el label="Toolbar" />
+                    </q-breadcrumbs>
+
+                    <q-space />
+
+                    <q-btn flat round dense icon="message" class="q-mr-xs" />
+                    <q-btn flat round dense icon="avatar" />
+                    </q-toolbar>
+                </q-header>
+                </div>
+                <div class="col">
+                    .col
+                </div>
+            </div>
+        </div>
+    </q-page>
+</template>
+
+<style>
+
+</style>
